14b06cddc123dde4e0cd7541d22997899db40c0a
Sushant Sanjay Khadilkar <37959802+sushantk1212@users.noreply.github.com>
2024-11-04 20:40:24 +0530

    Add files via upload

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MarvellousLabel' title`, () => {
+  it(`should have the 'DemoApplication' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MarvellousLabel');
+    expect(app.title).toEqual('DemoApplication');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MarvellousLabel');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DemoApplication');
   });
 });

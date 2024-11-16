b582cc7430f1e09d93eadb09e6b18ed2c10f1fd4
Sushant Sanjay Khadilkar <37959802+sushantk1212@users.noreply.github.com>
2024-11-16 11:48:22 +0530

    Solution 4 files

---
+++ Solution-4/src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-comp.component.html',
+  styleUrl: './new-comp.component.css'
+})
+export class NewCompComponent {
+  Name: string = "Marvellous";
+  NewName : string = "Infosystems";
+}
